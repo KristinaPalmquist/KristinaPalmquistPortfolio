adb4eb5fc600fb7740e3113a41a1832f7bffebd4
Kristina <kristina.palmquist@gmail.com>
2025-06-11 16:53:14 +0200

    Setting up netlify forms

--- js/index.js
+++ js/index.js
@@ -149,6 +149,28 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// FORM SUBMISSION
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
+
 // SCROLL TO TOP
 document.addEventListener("DOMContentLoaded", function () {
   const scrollBtn = document.getElementById(
